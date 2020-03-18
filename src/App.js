de7bf7f3c91c8715a5911c4a76a3dee6b072a126
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2020-03-18 00:11:07 -0300

    fix eslint

--- src/App.js
+++ src/App.js
@@ -10,31 +10,31 @@ import Home from './pages/Home';
 import PrivateRoute from './PrivateRoute';
 import { AuthContext } from './context/auth';
 
-function App(props) {
-    const existingTokens = JSON.parse(localStorage.getItem("tokens"));
+function App (props) {
+    const existingTokens = JSON.parse(localStorage.getItem('tokens'));
     const [authTokens, setAuthTokens] = useState(existingTokens);
 
-    const setTokens = (data) => {
-        localStorage.setItem("tokens", JSON.stringify(data));
+    const setTokens = data => {
+        localStorage.setItem('tokens', JSON.stringify(data));
         setAuthTokens(data);
-      }
-        return (
-            <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/login" component={Login} />
-                        <Route path="/signup" component={SignUp} />
-                        <PrivateRoute exact path="/my-account" component={MyAccount} />
-                        <PrivateRoute
-                            exact
-                            path="/terms-and-conditions"
-                            component={TermsAndConditions}
-                        />
-                        <PrivateRoute path="/welcome" component={Welcome} />
-                    </Switch>
-                </Router>
-            </AuthContext.Provider>
-        );
+    };
+    return (
+        <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route path="/signup" component={SignUp} />
+                    <PrivateRoute exact path="/my-account" component={MyAccount} />
+                    <PrivateRoute
+                        exact
+                        path="/terms-and-conditions"
+                        component={TermsAndConditions}
+                    />
+                    <PrivateRoute path="/welcome" component={Welcome} />
+                </Switch>
+            </Router>
+        </AuthContext.Provider>
+    );
 }
 export default App;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Home (props) {
     return <h1>Home</h1>;
 }
-
 export default Home;
